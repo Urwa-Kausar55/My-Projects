47793ce9432f73654474e74ef4e35a75bf8d6167
Urwa_Kausar <urwakausar55@gmail.com>
2026-07-11 01:35:26 +0500

    Add files via upload

---
+++ End-to-End-Netflix-Analytics/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE netflix_data_table
+(
+show_id VARCHAR(50),
+type VARCHAR(50),
+title TEXT,
+director TEXT,
+"cast" TEXT,
+country	TEXT,
+date_added TEXT,
+release_year INT,
+rating VARCHAR(50),
+duration VARCHAR(50),
+listed_in TEXT,
+description TEXT
+);
